e7fb6cedda54ba0cf43a2f8a9bae5028aacabbe4
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-13 14:07:43 +0700

    modify create barang btn

--- app/Filament/Resources/BarangResource/Pages/ListBarangs.php
+++ app/Filament/Resources/BarangResource/Pages/ListBarangs.php
@@ -13,7 +13,8 @@ class ListBarangs extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make('create')
+                ->icon('heroicon-m-plus')->label('Tambah Barang')
         ];
     }
 }
