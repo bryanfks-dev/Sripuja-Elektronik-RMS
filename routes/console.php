8f1a63a91434dc181b4b71ecc8dc5badd4bed025
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-18 12:39:12 +0700

    optimize query

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,7 @@ Schedule::call(function () {
         DB::beginTransaction();
 
         DB::table('karyawans')
-        ->update(['gaji_bln_ini' => DB::raw('gaji')]);
+            ->update(['gaji_bln_ini' => DB::raw('gaji')]);
 
         DB::commit();
     } catch (Halt $exception) {
@@ -29,28 +29,24 @@ Schedule::call(function () {
 Schedule::call(function () {
     $waktuMasuk = ConfigJson::loadJson()['waktu_masuk'];
 
-    $notPresents =
-        DB::table('karyawans')
-            ->leftJoin('absensis', function($join) {
-                $today = date('Y-m-d');
-
-                $join->on('absensis.karyawan_id', '=', 'karyawans.id')
-                    ->on(DB::raw("DATE(absensis.tanggal_waktu)"), '=', DB::raw("DATE('$today')"));
-            })
-            ->whereNull('absensis.tanggal_waktu')
-            ->orWhere(DB::raw("TIME(absensis.tanggal_waktu)"), '>', DB::raw("TIME('$waktuMasuk')"))
-            ->get('karyawans.id')
-            ->map(fn ($val) => $val->id)->toArray();
-
     try {
         DB::beginTransaction();
 
         // Subtract Rp 50_000 from karyawans
-        DB::table('karyawans')->whereIn('id', $notPresents)
-        ->update([
-            'gaji_bln_ini' => DB::raw('CASE WHEN `gaji_bln_ini` > 50000 THEN
+        DB::table('karyawans')->whereIn('id', DB::table('karyawans')
+                ->select('karyawans.id')
+                ->leftJoin('absensis', function ($join) {
+                    $today = date('Y-m-d');
+
+                    $join->on('absensis.karyawan_id', '=', 'karyawans.id')
+                        ->on(DB::raw("DATE(absensis.tanggal_waktu)"), '=', DB::raw("DATE('$today')"));
+                })
+                ->whereNull('absensis.tanggal_waktu')
+                ->orWhere(DB::raw("TIME(absensis.tanggal_waktu)"), '>', DB::raw("TIME('$waktuMasuk')")))
+            ->update([
+                'gaji_bln_ini' => DB::raw('CASE WHEN `gaji_bln_ini` > 50000 THEN
                 (`gaji_bln_ini` - 50000) ELSE `gaji_bln_ini` END'),
-        ]);
+            ]);
 
         DB::commit();
     } catch (Halt $exception) {
@@ -65,5 +61,5 @@ Schedule::call(function () {
         throw $exception;
     }
 })->dailyAt('23:30')->when(
-    fn () => ConfigJson::loadJson()['otomasi']
-);
+        fn() => ConfigJson::loadJson()['otomasi']
+    );
