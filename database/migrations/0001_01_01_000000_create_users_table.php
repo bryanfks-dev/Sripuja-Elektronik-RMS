6aa2fdc08b2a5def4717259effe78d1891011845
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-28 09:28:00 +0700

    add indexing to username

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('username')->index()->unique();
             $table->string('email')->nullable()->unique();
             $table->string('password');
 
